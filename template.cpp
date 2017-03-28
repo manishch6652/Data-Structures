077c63ac519aac78f4a5b082553da9351149d30c
manish <manishch6652@gmail.com>
2017-03-28 23:42:33 +0530

    Intial commit

---
+++ template.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+#include<vector>
+#include<set>
+#include<map>
+#include<list>
+#include<stack>
+#include<queue>
+#include<utility>
+#define s(x) scanf("%lld",&x)
+#define s2(x,y) scanf("%lld %lld",&x,&y)
+#define ol(x) printf("%lld\n",x);
+#define ol2(x,y) printf("%lld %lld",x,y);
+#define P(x) printf("%lld\n",x)
+#define all(v) v.begin(),v.end()
+#define mp make_pair
+#define pb push_back
+#define FF first
+#define SS second
+#define mem(a,x)   memset(a,x,sizeof(a))
+#define mod   1000000007 
+#define forp(i,a,b) for(long long i = a;i <= b;i++)
+#define forn(i,b,a) for(long i = b;i => a;i--)
+#define forip(it,contr) for(auto it = contr.begin(); it != contr.end();it++)
+#define SPEEDUP ios::sync_with_stdio(false)
+using namespace std;
+typedef long long int LL;
+typedef unsigned long long ULL;
+typedef pair<LL, LL>  pii;
+typedef vector<LL >  vi;
+int main()
+{
+    SPEEDUP;
+	return 0;
+	
+} 
